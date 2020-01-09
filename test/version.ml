b33a4f9753b15fde8a5c8aacb1d0dcc1ac9e71ec
Anton Bachin <antonbachin@yahoo.com>
2020-01-09 07:38:33 +0300

    Upgrade libuv to 1.34.0

--- test/version.ml
+++ test/version.ml
@@ -9,10 +9,10 @@ let tests = [
       Alcotest.(check int) "number" 1 Luv.Version.major);
 
     "minor", `Quick, (fun () ->
-      Alcotest.(check int) "number" 33 Luv.Version.minor);
+      Alcotest.(check int) "number" 34 Luv.Version.minor);
 
     "patch", `Quick, (fun () ->
-      Alcotest.(check int) "number" 1 Luv.Version.patch);
+      Alcotest.(check int) "number" 0 Luv.Version.patch);
 
     "is_release", `Quick, (fun () ->
       Alcotest.(check bool) "value" true Luv.Version.is_release);
@@ -21,12 +21,12 @@ let tests = [
       Alcotest.(check string) "suffix" "" Luv.Version.suffix);
 
     "hex", `Quick, (fun () ->
-      Alcotest.(check int) "number" 0x012101 Luv.Version.hex);
+      Alcotest.(check int) "number" 0x012200 Luv.Version.hex);
 
     "version", `Quick, (fun () ->
-      Alcotest.(check int) "number" 0x012101 (Luv.Version.version ()));
+      Alcotest.(check int) "number" 0x012200 (Luv.Version.version ()));
 
     "string", `Quick, (fun () ->
-      Alcotest.(check string) "value" "1.33.1" (Luv.Version.string ()));
+      Alcotest.(check string) "value" "1.34.0" (Luv.Version.string ()));
   ]
 ]
