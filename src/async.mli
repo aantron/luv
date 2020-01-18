fcd65cc386877b22a381e4a54b450f13bac60d84
Anton Bachin <antonbachin@yahoo.com>
2020-01-18 12:02:46 +0300

    API docs progress

--- src/async.mli
+++ src/async.mli
@@ -3,7 +3,30 @@
 
 
 
+(** Inter-thread communication.
+
+    See {{:http://docs.libuv.org/en/v1.x/async.html} [uv_async_t] {i - Async
+    handle}}. *)
+
 type t = [ `Async ] Handle.t
+(** Binds {{:http://docs.libuv.org/en/v1.x/async.html#c.uv_async_t}
+    [uv_async_t]}.
+
+    Note that values of this type can be passed to functions in {!Luv.Handle},
+    in addition to the functions in this module. In particular, see
+    {!Luv.Handle.close}. *)
 
 val init : ?loop:Loop.t -> (t -> unit) -> (t, Error.t) result
+(** Allocates and initializes an async handle.
+
+    Binds {{:http://docs.libuv.org/en/v1.x/async.html#c.uv_async_init}
+    [uv_async_init]}.
+
+    The handle should be cleaned up with {!Luv.Handle.close} when no longer
+    needed. *)
+
 val send : t -> (unit, Error.t) result
+(** Triggers a call to the handle's callback by the handle's loop.
+
+    Binds {{:http://docs.libuv.org/en/v1.x/async.html#c.uv_async_send}
+    [uv_async_send]}. *)
--- src/buffer.mli
+++ src/buffer.mli
@@ -3,28 +3,101 @@
 
 
 
+(** Data buffers. *)
+
+
+
+(** {1 Basics} *)
+
 type t = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+(** Buffers with C storage.
+
+    These are OCaml bigstrings ([char] bigarrays). Roughly speaking, they
+    correspond to C buffers referenced by pointers of type [char*], but also
+    know their own size.
+
+    In addition to being usable with the functions in this module, the type is
+    compatible with at least the following libraries:
+
+    - {{:https://caml.inria.fr/pub/docs/manual-ocaml/libref/Bigarray.Array1.html}
+      [Bigarray]} from OCaml's standard library
+    - {{:https://ocsigen.org/lwt/dev/api/Lwt_bytes} [Lwt_bytes]} from Lwt
+    - {{:https://github.com/inhabitedtype/bigstringaf} bigstringaf}
+    - {{:https://github.com/c-cube/ocaml-bigstring} ocaml-bigstring}. *)
 
 val create : int -> t
+(** Allocates a fresh buffer of the given size. *)
+
 val size : t -> int
+(** Evaluates to the size of the given buffer. *)
+
 val get : t -> int -> char
-val set : t -> int -> char -> unit
-val sub : t -> offset:int -> length:int -> t
-val blit : source:t -> destination:t -> unit
-val fill : t -> char -> unit
+(** [Luv.Buffer.get buffer index] retrieves the character in [buffer] at
+    [index].
+
+    Can also be written as [buffer.{index}]. *)
+
 val unsafe_get : t -> int -> char
+(** Like {!Luv.Buffer.get}, but does not perform a bounds check. *)
+
+val set : t -> int -> char -> unit
+(** [Luv.Buffer.set buffer index value] sets the character in [buffer] at
+    [index] to [value].
+
+    Can also be written as [buffer.{index} <- value]. *)
+
 val unsafe_set : t -> int -> char -> unit
+(** Like {!Luv.Buffer.set}, but does not perform a bounds check. *)
+
+val sub : t -> offset:int -> length:int -> t
+(** [Luv.Buffer.sub buffer ~offset ~length] creates a view into [buffer] that
+    starts at the given offset and has the given length.
+
+    No data is copied. *)
+
+val blit : source:t -> destination:t -> unit
+(** Copies data from [source] to [destination].
+
+    The amount of data copied is the minimum of the two buffers' size.
+
+    To copy part of a buffer, use {!Luv.Buffer.sub} to create a view, and pass
+    the view to [Luv.Buffer.blit]. *)
+
+val fill : t -> char -> unit
+(** Fills the given buffer with the given character. *)
+
+
+
+(** {1 Conversions} *)
 
 val to_string : t -> string
+(** Creates a string with the same contents as the given buffer. *)
+
 val to_bytes : t -> bytes
+(** Creates a [bytes] buffer with the same contents as the given buffer. *)
+
 val from_string : string -> t
+(** Creates a buffer from a string. *)
+
 val from_bytes : bytes -> t
+(** Creates a buffer from [bytes]. *)
+
+
+
+(** {1 Converting blits} *)
 
 val blit_to_bytes : t -> bytes -> destination_offset:int -> unit
-val blit_from_bytes : t -> bytes -> source_offset:int -> unit
-val blit_from_string : t -> string -> source_offset:int -> unit
+(** Copies data from a buffer to a [bytes] buffer. *)
 
-(* DOC How to use sub, sub+blit. *)
+val blit_from_bytes : t -> bytes -> source_offset:int -> unit
+(** Copies data from a [bytes] buffer to a buffer. *)
+
+val blit_from_string : t -> string -> source_offset:int -> unit
+(** Copies data from a string to a buffer. *)
+
+
+
+(** {1 Lists of buffers} *)
 
 module List :
 sig
