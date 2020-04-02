a67ac247e653b58f273610b10f464249b5d9afb3
Glenn Slotte <glenn@slotte.net>
2020-04-02 14:25:29 +0200

    add change test to FS_event (#55)

--- test/FS_event.ml
+++ test/FS_event.ml
@@ -67,5 +67,39 @@ let tests = [
         Alcotest.(check bool) "occurred" true !occurred
       end
     end;
+
+    "change", `Quick, begin fun () ->
+      with_fs_event begin fun event ->
+        let occurred = ref false in
+
+        open_out filename |> close_out;
+
+        Luv.FS_event.start event filename begin fun result ->
+          Luv.FS_event.stop event |> check_success_result "stop";
+          let filename', events = check_success_result "start" result in
+          Alcotest.(check string) "filename" filename filename';
+          Alcotest.(check bool) "rename" false (List.mem `RENAME events);
+          Alcotest.(check bool) "change" true (List.mem `CHANGE events);
+          occurred := true
+        end;
+
+        let start = ref 0. in
+
+        let timer = Luv.Timer.init () |> check_success_result "timer init" in
+        check_success_result "timer start" @@
+        Luv.Timer.start timer 100 begin fun () ->
+          start := Unix.gettimeofday ();
+          let oc = open_out filename in
+          let () = Printf.fprintf oc "foo" in
+          close_out oc
+        end;
+
+        run ();
+
+        Alcotest.(check bool) "occurred" true !occurred;
+        Alcotest.(check (float 0.1)) "delay < 100ms" 0.
+          (Unix.gettimeofday () -. !start)
+      end
+    end;
   ]
 ]
