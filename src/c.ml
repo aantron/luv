ecafee59698ee2973158733fa5fea0270251b8ae
Etienne Millon <me@emillon.org>
2022-12-14 12:10:59 +0100

    ctypes port (WIP)

--- src/c.ml
+++ src/c.ml
@@ -3,10 +3,6 @@
 
 
 
-module Types = Luv_c_types
-module Functions =
-  Luv_c_function_descriptions.Descriptions
-    (Luv_c_generated_functions.Non_blocking)
-module Blocking =
-  Luv_c_function_descriptions.Blocking
-    (Luv_c_generated_functions.Blocking)
+module Types = Luv_c.Types_generated
+module Functions = Luv_c.Luv_c_generated.Non_blocking_inst
+module Blocking = Luv_c.Luv_c_generated.Blocking_inst
