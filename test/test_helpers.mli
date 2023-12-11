f52af4c564ae1be7447076c43c572bec2769ad58
Anton Bachin <antonbachin@yahoo.com>
2023-12-11 10:01:12 +0300

    Adapt tester to removal of result dependency
    See 8b368a9893a343783542653b1b830fcf8927a880.

--- test/test_helpers.mli
+++ test/test_helpers.mli
@@ -3,11 +3,11 @@
 
 
 
-val check_success_result : string -> ('a, Luv.Error.t) Result.result -> 'a
+val check_success_result : string -> ('a, Luv.Error.t) result -> 'a
 val check_error_result :
-  string -> Luv.Error.t -> (_, Luv.Error.t) Result.result -> unit
+  string -> Luv.Error.t -> (_, Luv.Error.t) result -> unit
 val check_error_results :
-  string -> Luv.Error.t list -> (_, Luv.Error.t) Result.result -> unit
+  string -> Luv.Error.t list -> (_, Luv.Error.t) result -> unit
 val check_error_code : string -> Luv.Error.t -> Luv.Error.t -> unit
 
 val check_not_null : string -> _ Ctypes.ptr -> unit
